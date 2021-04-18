3662ea10868fcd8a986ea088df7d89e4968401bd
Lucio Delelis <ldelelis@est.frba.utn.edu.ar>
2021-04-18 18:51:09 -0300

    fix(core): corrects build environment, port mapping

--- packages/core/src/app.controller.ts
+++ packages/core/src/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpCode } from '@nestjs/common';
 import { AppService } from './app.service';
+
+interface Healthcheck {
+  status: string
+}
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('healthcheck')
+  @HttpCode(200)
+  healthcheck(): Healthcheck {
+    return {status: "OK"};
   }
 }
