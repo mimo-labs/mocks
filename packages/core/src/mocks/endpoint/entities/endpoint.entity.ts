6cc6f91fd693fbac3fba555da562de4ed3809006
Lucio Delelis <ldelelis@est.frba.utn.edu.ar>
2021-04-21 19:33:18 -0300

    feat(core): models endpoint entity

--- packages/core/src/mocks/endpoint/entities/endpoint.entity.ts
+++ packages/core/src/mocks/endpoint/entities/endpoint.entity.ts
@@ -1,1 +1,17 @@
-export class Endpoint {}
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { MaxLength, Matches } from 'class-validator';
+
+import { PATH_REGEX } from 'common/constants';
+
+@Entity()
+export class Endpoint {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ default: true })
+  @MaxLength(2048, {
+    message: "Path is too long. Maximum length: 2048"
+  })
+  @Matches(PATH_REGEX)
+  path: string = '/';
+}
