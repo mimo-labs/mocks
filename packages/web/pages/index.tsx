de076c5de4c86422cb21fbd07f66f59dc7f80024
niconiahi <nicolas.accetta@gmail.com>
2021-04-24 12:39:00 -0300

    fea(web): add initial eslint and prettier configuration

---
+++ packages/web/pages/index.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from "react";
+import Head from "next/head";
+
+const Home: FC = () => (
+  <div>
+    <Head>
+      <title>Mimo mocks</title>
+    </Head>
+    <h1>Im the home</h1>
+  </div>
+);
+
+export default Home;
